8fdbc16194bc289f69242c877f7bb82580b02e68
Rafael Valdez <rvaldez@energyogre.com>
2021-02-09 08:55:00 -0600

    changes in memory

--- OdeToFood.Data/Models/Services/InMemoryRestaurantData.cs
+++ OdeToFood.Data/Models/Services/InMemoryRestaurantData.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OdeToFood.Data.Models.Services
 {
     public class InMemoryRestaurantData : IRestaurantData
     {
+
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant {Id = 1 , Name = "Rafa's Pizza", Cuisine = CuisineType.Italian},
+                    new Restaurant {Id = 2 , Name = "Ashley's Masala", Cuisine = CuisineType.Indian},
+                    new Restaurant {Id = 3 , Name = "Kelly's Baggete", Cuisine = CuisineType.French}
+                };   
+        }           
         public IEnumerable<Restaurant> GetAll()
         {
-            throw new NotImplementedException();
+            return restaurants.OrderBy(r => r.Name);
         }
     }
 }
